97e6ef235fea41a7cd83cfe6569f4a8b71113556
ccarvalhojib <ccarvalho@jib-home.com>
2020-06-19 10:06:15 -0300

    add line 222

--- app/src/main/java/com/example/cfctestjava/MainActivity.java
+++ app/src/main/java/com/example/cfctestjava/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        println("Add line 111");
+        println("Add line 222");
     }
 }
